ee82f2506095cdeae87d1ac9eb8b88784ae10c0d
danielcobo <danielcobo@users.noreply.github.com>
2016-12-20 12:04:52 +0100

    Rewrite mkTree

--- mktree.js
+++ mktree.js
@@ -1,32 +1,41 @@
-var fs = require("fs");
+'use strict';
 
-var mkTree = function(toPath, dirs, index, callback){
-  var i = 0;
-  var finallyDo = callback;
-  if (typeof index === "function"){
-    finallyDo = index;
-  }
-  else if (index){
-    i = index;
-  }
+var fs = require('fs');
+var sortPaths = require('./sortPaths.js');
 
-  fs.mkdir(toPath+dirs[i], function(err){
-                      if (err){
-                        throw err;
-                      }
-                      else{
-                        i++;
-                        if (i<dirs.length){
-                          mkTree(toPath,dirs,i,finallyDo);
-                        }
-                        else if (typeof callback === "function"){
-                          finallyDo();
-                        }
-                        else{
-                          //silent fail?
-                        }
-                      }
-                    });
+var mkTree = function(toPath, dirs, callback, i){
+	if (typeof i !== 'number'){
+		i = 0;
+	}
+	
+	if(typeof toPath !== 'string'){
+		throw new Error('Error: Invalid param - toPath must be a string');
+	}
+	
+	if (dirs === 'string'){
+		dirs = [dirs];
+	}
+	if (!dirs.length){
+		throw new Error ('Error: Invalid param - dirs length is 0');
+	}
+	
+	//Sort by depth (shallow first)
+	if (i===0){
+		dirs = sortPaths();
+	}
+
+	fs.mkdir(toPath+dirs[i], function(err){
+		if (err){
+			throw err;
+		}
+		i++;
+		if (i<dirs.length){
+			mkTree(toPath,dirs,callback,i);
+		}
+		else if (typeof callback === 'function'){
+			callback();
+		}
+	});
 };
 
 module.exports = mkTree;
