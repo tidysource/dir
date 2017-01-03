3ab5f2f113888208c35c4f276f238a3d697fcd9a
danielcobo <danielcobo@users.noreply.github.com>
2017-01-03 22:25:02 +0100

    Fix typo and Add colorize

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
 'use strict';
 
 var test = require('tape');
+var colorize = require('tap-colorize');
+var colorizeOptions = {
+	pass : '#B2D9B5',
+	fail : '#FE5A4E',
+	info : '#EEEEEE'
+};
+test.createStream().pipe(colorize(colorizeOptions)).pipe(process.stdout);
+
 var dir = require('../index.js');
 
-test('sortPaths()', function (assert) {
+test('byDepth()', function (assert) {
     assert.plan(3);
 	
 	var paths = [
@@ -17,5 +25,5 @@ test('sortPaths()', function (assert) {
 
 	assert.deepEqual(depth[0], ['hello']);
 	assert.deepEqual(depth[1], ['foo/bar','hello/world']);
-	assert.deepEqual(depth[2], ['hello/world/tidy/']);
+	assert.deepEqual(depth[3], ['hello/world/tidy/']); //should be depth 2<--- do this by removing any trailing slash? <--- or note it's sensitive to trailing slash
 });
