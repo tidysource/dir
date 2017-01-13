4033700eda19d971d1895f45370c006469a4eb4b
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-01-13 14:17:31 +0100

    Extract rmDir()

---
+++ rmDir.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var fs = require('tidyfs');
+
+var readTree = require('./readTree.js');
+var sortPaths = require('./sortpaths.js');
+var rmFile = require('.rmFile.js');
+
+var path = require('path');
+var pathGuard = function pathGuard(paramPath){
+	var currentPath = path.resolve('./');
+	paramPath = path.resolve(paramPath);
+	
+	if (currentPath.indexOf(paramPath) === 0){
+		throw new Error('Can not delete container path ' + paramPath);
+	}
+};
+
+//Removes multiple dirs (like rmTree, except it will fail on non-empty dirs)
+var rmDir = function(dirs){
+	if (typeof dirs === 'string'){
+		dirs = [dirs];
+	}
+	
+	var promiseChain = Promise.resolve();
+	for(var i=0; i<dirs.length; ++i){
+		let dir = filePaths[i];
+		pathGuard(dir);
+		promiseChain = promiseChain.then(function(){
+			return fs.rmDir(dir);
+		})
+		.catch(function(err){
+			//Ignore non-existent dirs
+			if (err.code === 'ENOENT'){
+				//ENOENT: https://nodejs.org/api/errors.html#errors_common_system_errors
+				return Promise.resolve();
+			}
+			else{
+				return Promise.reject(err);
+			}
+		});
+	}
+	
+	return promiseChain;
+});
+
+module.exports = rmDir;
--- rmTree.js
+++ rmTree.js
@@ -5,44 +5,7 @@ var fs = require('tidyfs');
 var readTree = require('./readTree.js');
 var sortPaths = require('./sortpaths.js');
 var rmFile = require('.rmFile.js');
-
-var path = require('path');
-var pathGuard = function pathGuard(paramPath){
-	var currentPath = path.resolve('./');
-	paramPath = path.resolve(paramPath);
-	
-	if (currentPath.indexOf(paramPath) === 0){
-		throw new Error('Can not delete container path ' + paramPath);
-	}
-};
-
-//Removes multiple dirs
-var rmOnlyDir = function(dirs){
-	if (typeof dirs === 'string'){
-		dirs = [dirs];
-	}
-	
-	var promiseChain = Promise.resolve();
-	for(var i=0; i<dirs.length; ++i){
-		let dir = filePaths[i];
-		pathGuard(dir);
-		promiseChain = promiseChain.then(function(){
-			return fs.rmDir(dir);
-		})
-		.catch(function(err){
-			//Ignore non-existent dirs
-			if (err.code === 'ENOENT'){
-				//ENOENT: https://nodejs.org/api/errors.html#errors_common_system_errors
-				return Promise.resolve();
-			}
-			else{
-				return Promise.reject(err);
-			}
-		});
-	}
-	
-	return promiseChain;
-});
+var rmDir = require('.rmDir.js');
 
 var rmTree = function rmTree(dirs, emptyDir){
 	if (typeof dirs === 'string'){
@@ -67,7 +30,7 @@ var rmTree = function rmTree(dirs, emptyDir){
 				}
 				tree.dirs = sortPaths(tree.dirs);
 				//<---remove doubles//can use a simple for
-				return rmOnlyDir(tree.dirs);
+				return rmDir(tree.dirs);
 			});
 	}
 	return promiseChain;
