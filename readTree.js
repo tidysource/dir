3ddc610dc9666ffce5f993a479b7be27dcdddb49
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-01-15 16:01:05 +0100

    Returns same as list tree but files array contains file objects instead of paths

--- readTree.js
+++ readTree.js
@@ -1,101 +1,23 @@
 'use strict';
 
 var fs = require('tidyfs');
+var listTree = require('listTree');
 
-//Read single dirPath
-var listTree = function listTree(dir, callback, toCheck, i, result){
-	if (typeof toCheck !== defined){	
-		if (typeof dir !== 'string'){
-			throw new Error('Invalid parameter: dir must be a string');
-		}
-		toCheck = [dir];
-	}
-	if (typeof i !== 'number'){
-		i = 0;
-	}
-	if (typeof result !== 'object'){
-		result = {
-			dirs : [],
-			files : []
-		};
-	}
-
-	var checkPath = toCheck[i];	
-	fs.stat(checkPath)
-		.then(function(stats){
-			if (stats.isFile()){
-				result.files.push(checkPath);
-			}
-			else if (stats.isDirectory()){
-				result.dirs.push(checkPath);
-				return fs.readDir(checkPath);
-			}
-			//else is not a file nor dir
+var readTree = function readTree(dirs){
+	return listTree(dirs)
+		.then(function(tree){
+			//reaturn tree and readFIles
+			return Promise.all([
+								Promise.resolve(tree),
+								fs.readFile(tree.files)
+								]);
 		})
-		.then(function(dirContent){
-			if (typeof dirContent !== 'undefined'){
-				for(var i=0; i<dirContent.length; ++i){
-					var itemPath = dirContent[i];
-					toCheck.push(itemPath);
-				}
+		.then(function(vals){
+			return {
+				dirs : vals[0].dirs,
+				files : vals[1]
 			}
-		})
-		.then(function(){
-			if (i < toCheck.length){
-				++i;
-				listTree(dir, callback, toCheck, i, result);
-			}
-			else{
-				callback(null,result);
-			}
-		})
-		.catch(function(err){
-			callback(err, result)	
 		});
-};
-
-//Promisify listTree
-var listTreePromise = function listTreePromise(dirPath) {
-	return new Promise(function(resolve, reject){
-		listTree(
-			dirPath, 
-			function(result){
-				resolve(result);
-			});
-	});
-};
-
-//Read single/array of dir/s
-var readTree = function readTree(dirs,separate){
-	var inputWasString = false;
-	if (typeof dirs === 'string'){
-		inputWasString = true;
-		dirs = [dirs];
-	}
-	
-	var dirsContent = {};
-	var promiseChain = Promise.resolve();
-	for(var i=0; i<dirs; ++i){
-		let dir = dirs[i];
-		promiseChain = promiseChain
-			.then(function(){
-				return listTreePromise(dir);
-			})
-			.then(function(result){
-				//result gives dirs and files within current dir
-				if (inputWasString){
-					return result;
-				}
-				else if (separate){
-					dirsContent[dir] = result;
-					return dirsContent;	
-				}
-				else{
-					//else join all into file array and dirs array and return
-				}
-			});
-	}
-	return promiseChain;
 };
 
 module.exports = readTree;
