8ea1a3dc176895af67c173bf4177b8a6a65a7891
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-03-15 12:01:44 +0100

    Reduce to basics

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+'use strict';
+
 module.exports = {
 	mkTree : require('./mkTree.js'),
 	mkFile : require('./mkFile.js'),
 	listTree : require('./listTree.js'),
-	readTree : require('./readTree.js'),
-	readFile : require('./readFile.js'),
-	byDepth : require('./byDepth.js'),
-	sortPaths : require('./sortPaths.js'),
-	fileExt : require('./fileExt.js'),
-	rmExt : require('./rmExt.js'),
 	rmFile : require('./rmFile.js'),
 	rmDir : require('./rmDir.js'),
 	rmTree : require('./rmTree.js'),
-	emptyTree : require('./emptyTree.js')
+	emptyTree : require('./emptyTree.js'),
+	readTree : require('./readTree.js'),
+	readFile : require('./readFile.js')
 };
