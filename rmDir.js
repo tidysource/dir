586f27029c55efee1fe6921867c8eef20a8bb042
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-03-15 12:04:17 +0100

    Remove unnecessary code

--- rmDir.js
+++ rmDir.js
@@ -2,19 +2,6 @@
 
 var fs = require('tidyfs');
 
-var sortPaths = require('./sortpaths.js');
-var rmFile = require('.rmFile.js');
-
-var path = require('path');
-var pathGuard = function pathGuard(paramPath){
-	var currentPath = path.resolve('./');
-	paramPath = path.resolve(paramPath);
-	
-	if (currentPath.indexOf(paramPath) === 0){
-		throw new Error('Can not delete container path ' + paramPath);
-	}
-};
-
 //Removes multiple dirs (like rmTree, except it will fail on non-empty dirs)
 var rmDir = function(dirs){
 	if (typeof dirs === 'string'){
@@ -23,15 +10,17 @@ var rmDir = function(dirs){
 	
 	var promiseChain = Promise.resolve();
 	for(var i=0; i<dirs.length; ++i){
-		let dir = filePaths[i];
-		pathGuard(dir);
+		let dir = dirs[i];
 		promiseChain = promiseChain.then(function(){
 			return fs.rmDir(dir);
 		})
 		.catch(function(err){
 			//Ignore non-existent dirs
 			if (err.code === 'ENOENT'){
-				//ENOENT: https://nodejs.org/api/errors.html#errors_common_system_errors
+				/*
+				ENOENT:
+				https://nodejs.org/api/errors.html#errors_common_system_errors
+				*/
 				return Promise.resolve();
 			}
 			else{
@@ -41,6 +30,6 @@ var rmDir = function(dirs){
 	}
 	
 	return promiseChain;
-});
+};
 
 module.exports = rmDir;
